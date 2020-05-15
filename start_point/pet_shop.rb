2ddf3b1adf394bf90f0c6535baa7532db9b24e48
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-15 13:12:21 +0100

    Add function to increase number of pets sold

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -15,3 +15,7 @@ def pets_sold(pet_shop)
     return pet_shop[:admin][:pets_sold]
 end
 
+def increase_pets_sold(pet_shop, number_of_new_pet_sales)
+    total_pets_sold = pet_shop[:admin][:pets_sold] += number_of_new_pet_sales
+    return total_pets_sold
+end

a02cf138d4b28cc44e272f7707803c4f5d5e3b97
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-16 11:40:58 +0100

    Add sell pet to customer function

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -22,7 +22,7 @@ end
 
 def stock_count(pet_shop)
     return pet_shop[:pets].length
-end
+end  
 
 def pets_by_breed(pet_shop, breed)
     same_breed = []
@@ -34,6 +34,18 @@ def pets_by_breed(pet_shop, breed)
     return same_breed
 end
 
+# def find_pet_by_name(pet_shop, name)
+#     pets_found = []
+
+#     for pet in pet_shop[:pets]
+#         pets_found << pet if pet[:name] == name
+#         return pets_found
+#     end
+
+#     return nil if pets_found.length == 0
+# end
+
+# Working! V
 def find_pet_by_name(pet_shop, name)
 
     for pet in pet_shop[:pets]
@@ -65,11 +77,34 @@ def customer_pet_count(customers)
     return customers[:pets].length
 end
 
-def add_pet_to_customer(customers, new_pet)
-    return customers[:pets].push(new_pet).length
+def add_pet_to_customer(customers, pet)
+    return customers[:pets].push(pet).length
 end
 
-def customer_can_afford_pet(customers, new_pet)
-    return customers[:cash] >= new_pet[:price]
+def customer_can_afford_pet(customers, pet)
+    return customers[:cash] >= pet[:price]
 end
 
+def sell_pet_to_customer(pet_shop, pet, customers)
+    
+    add_pet_to_customer(customers, pet)
+    #Attempt 1: Does't increase no of pet sales...
+    # number_of_new_pet_sales = 0
+
+    # if find_pet_by_name(pet_shop, pet) != nil
+    #     number_of_new_pet_sales += 1
+    # end
+
+    # Attempt 2: Works
+    number_of_new_pet_sales = [find_pet_by_name(pet_shop, pet)]
+    
+    increase_pets_sold(pet_shop, number_of_new_pet_sales.length)
+
+    remove_customer_cash(customers, pet[:price])
+
+    add_or_remove_cash(pet_shop, pet[:price])
+
+end
+
+# in sell_pet_to_customer: check length of array from find pet to get the number
+# Need a way to turn the pet (Arthur) into number (1)
